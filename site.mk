b254e1b20b416b40613a310d08d020fc63ec6c5c
Elstrueck <christian@struecken.de>
2017-11-09 20:11:24 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,30 +1,26 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-        gluon-respondd \
-        gluon-autoupdater \
-        gluon-setup-mode \
-        gluon-config-mode-core \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-tunneldigger \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-web-admin \
-        gluon-web-autoupdater \
-        gluon-web-private-wifi \
-        gluon-web-network \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
 	gluon-web-wifi-config \
-        gluon-mesh-vpn-tunneldigger \
-        gluon-radvd \
-        gluon-status-page \
-        gluon-migrate-vpn \
-        gluon-tunneldigger-watchdog \
-        iwinfo \
-        iptables \
-        haveged
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iwinfo
 
 # Languages to include
 GLUON_LANGS ?= en de
