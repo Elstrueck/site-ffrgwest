6f92683aeb887bc07379acedfb1848e1dbf0d9e7
Elstrueck <christian@struecken.de>
2017-11-19 18:07:19 +0100

    update to gluon 2017.1.4 (experimental)

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 # Languages to include
 GLUON_LANGS ?= en de
 
-DEFAULT_GLUON_RELEASE := 0.9.3+exp+$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.9.4+exp+$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
