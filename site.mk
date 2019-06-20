abedb66341779b431089e630e598991a85c45914
Elstrueck <christian@struecken.de>
2019-06-20 16:30:46 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,31 +1,27 @@
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	mesh-vpn-fastd \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
+
 GLUON_SITE_PACKAGES := \
-	gluon-alfred \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-wifi-config \
-	gluon-mesh-batman-adv-15 \
+
 	gluon-mesh-vpn-tunneldigger \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
 	haveged \
 	iwinfo
 
 # Languages to include
 GLUON_LANGS ?= en de
 
-DEFAULT_GLUON_RELEASE := 0.9.8+exp+$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2018.2.1+exp+$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
@@ -38,4 +34,3 @@ GLUON_REGION ?= eu
 
 # Select ath10k Firmware for adhoc
 GLUON_ATH10K_MESH ?= 11s
-
